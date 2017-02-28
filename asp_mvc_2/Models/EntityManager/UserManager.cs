0108aa04ec43ea1abd63651bdedd68ed279ea431
Aulina <aulinarj@gmail.com>
2017-02-28 13:11:15 +0700

    Modul 4

--- asp_mvc_2/Models/EntityManager/UserManager.cs
+++ asp_mvc_2/Models/EntityManager/UserManager.cs
@@ -233,8 +233,37 @@ namespace asp_mvc_2.Models.EntityManager
                         dbContextTransaction.Rollback();
                     }
                 }
+
             }
         }
-
+        public UserProfileView GetUserProfile(int userID)
+        {
+            UserProfileView UPV = new UserProfileView();
+            using (DBEntities db = new DBEntities())
+            {
+                var user = db.SYSUsers.Find(userID);
+                if (user != null)
+                {
+                    UPV.SYSUserID = user.SYSUserID;
+                    UPV.LoginName = user.LoginName;
+                    UPV.Password = user.PasswordEncryptedText;
+                    var SUP = db.SYSUserProfiles.Find(userID);
+                    if (SUP != null)
+                    {
+                        UPV.FirstName = SUP.FirstName;
+                        UPV.LastName = SUP.LastName;
+                        UPV.Gender = SUP.Gender;
+                    }
+                    var SUR = db.SYSUserRoles.Find(userID);
+                    if (SUR != null)
+                    {
+                        UPV.LOOKUPRoleID = SUR.LOOKUPRoleID;
+                        UPV.RoleName = SUR.LOOKUPRole.RoleName;
+                        UPV.IsRoleActive = SUR.IsActive;
+                    }
+                }
+            }
+            return UPV;
+        }
     }
 }
